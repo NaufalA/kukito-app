832dc9fed6dd683c2f76214cd942b8ee9eec4247
NaufalA <naufal7600@gmail.com>
2026-09-19 17:43:10 +0700

    fix: add icons for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'kukito-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'kukito-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
